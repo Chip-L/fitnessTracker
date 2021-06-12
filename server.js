3e54f6a4c7d553f8da5302cfcd31dc504deb6e3a
Chip Long <chip_long@yahoo.com>
2021-06-11 22:54:37 -0600

    added in most basic of routes -- trying to understand models

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(controller);
+
 app.use(express.static("public"));
+app.use(controller);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
